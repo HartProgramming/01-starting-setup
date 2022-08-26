491b96380a55fe986e35017ca7ddd61ea7792e24
Web3Learner <programmedhart@protonmail.com>
2022-08-26 19:43:15 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Expenses from './components/Expenses';
 
 import NewExpense from './components/NewExpense/NewExpense';
 
+
 function App() {
   const expenses = [
     { title: 'Toilet Paper', amount: 299.99, date: new Date(2021, 2, 28) },
@@ -9,9 +10,14 @@ function App() {
     { title: 'Car Insurance', amount: 299.99, date: new Date(2021, 2, 28) }
   ]
 
+  const addExpenseHandler = (expense) => {
+    console.log('In App.js')
+    console.log(expense)
+  }
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler}/>
       <Expenses item={expenses} />
     </div>
   );
--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,14 +1,19 @@
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from './Card'
+import ExpensesFilter from './ExpensesFilter';
 
-function Expenses(props){
+function Expenses(props) {
+
     return (
-        <Card className='expenses'>
-            <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date}/>
-            <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date}/>
-            <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date}/>
-        </Card>
+        <div>
+            <ExpensesFilter />
+            <Card className='expenses'>
+                <ExpenseItem title={props.item[0].title} amount={props.item[0].amount} date={props.item[0].date} />
+                <ExpenseItem title={props.item[1].title} amount={props.item[1].amount} date={props.item[1].date} />
+                <ExpenseItem title={props.item[2].title} amount={props.item[2].amount} date={props.item[2].date} />
+            </Card>
+        </div>
     )
 }
 
